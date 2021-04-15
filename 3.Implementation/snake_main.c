4fd599aa8f66bb999e3adec2c6597bef298b92ea
gannurohith <49453539+gannurohith@users.noreply.github.com>
2021-04-15 21:23:31 +0530

    Create snake_main.c

---
+++ 3.Implementation/snake_main.c
@@ -0,0 +1,12 @@
+void main() {
+	while (true) {
+		printf("Set speed value for new game (1 - 100)\nSpeed : \n");
+		int a;
+		scanf_s("%d", &a);
+		if (a > 100) a = 100;
+		if (a < 1) a = 1;
+		Speed = (int) 1100 - a * 10;
+		GameHandler();
+	}
+	
+}
